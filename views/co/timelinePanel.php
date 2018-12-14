3b26a678122a2e52b53b4d0b7980a712a9d0e3ec
Bouboule <clement.damiens@gmail.com>
2018-12-14 09:18:50 +0100

    margin share

--- views/co/timelinePanel.php
+++ views/co/timelinePanel.php
@@ -292,7 +292,7 @@ if(@$media["object"]){
     
       <?php if(!empty(@$media["object"])){ ?>
       <div id="" data-pk="<?php echo $key ?>"  data-type="<?php echo @$media["object"]["type"]; ?>" 
-           class="col-md-12 no-padding margin-bottom-10 newsActivityStream<?php echo $media["object"]["id"]; ?>" 
+           class="col-xs-12 no-padding margin-bottom-10 margin-top-10 newsActivityStream<?php echo $media["object"]["id"]; ?>" 
                 <?php if(@$media["object"]["type"] == "news"){ ?>style="border:1px solid #E3E3E3;"<?php } ?>>
         <?php 
              if(@$media["object"]["type"] == "news")
