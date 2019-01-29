238d2a7de99cb08786153d35c0d5335f3d1af947
Oceatoon <oceatoon@gmail.com>
2019-01-29 10:50:02 +0400

    limitation etre loggué

--- controllers/actions/SaveAction.php
+++ controllers/actions/SaveAction.php
@@ -4,18 +4,24 @@ class SaveAction extends CAction
     public function run($type=null, $id= null)
     {
     	$controller=$this->getController();
-    	$result=News::save($_POST);
-    	if(!@$_GET["json"]){
-    		$params=array(
-    			"news"=>array( (string)$result["id"]=>$result["object"]), 
-    			"actionController"=>"save",
-    			"canManageNews"=>true,
-    			"canPostNews"=>true,
-                "endStream"=>false,
-                "pair" => false);
-			echo $controller->renderPartial("news.views.co.timelineTree", $params,true);
-    	}
-		else
-        	return Rest::json($result);
+        if ( Person::logguedAndValid() ) {
+        	$result=News::save($_POST);
+        	if(!@$_GET["json"]){
+        		$params=array(
+        			"news"=>array( (string)$result["id"]=>$result["object"]), 
+        			"actionController"=>"save",
+        			"canManageNews"=>true,
+        			"canPostNews"=>true,
+                    "endStream"=>false,
+                    "pair" => false);
+    			echo $controller->renderPartial("news.views.co.timelineTree", $params,true);
+        	}
+    		else
+            	return Rest::json($result);
+        } else {
+            $result = array("result" => false, "msg" => Yii::t("common","Please Login First")  );
+            return Rest::json($result);
+        }
+
     }
 }
